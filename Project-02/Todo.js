8ff7cd4b90c655ed2acde8da0b5fbc7d612e5ce6
ahmedd <ahmed.kib3@gmail.com>
2025-04-16 02:24:56 -0700

    Git Initialized

--- Project-02/Todo.js
+++ Project-02/Todo.js
@@ -1,17 +1,38 @@
 let inputs = document.getElementById("inp");
 let text = document.querySelector(".text");
 
-function Add(){
-    if(inputs.value == ""){
-        alert("Please Enter Task")
-    }else{
+function Add() {
+    if (inputs.value == "") {
+        alert("Please Enter Task");
+    } else {
         let newEle = document.createElement("ul");
-        newEle.innerHTML=`${inputs.value} <i class="fa-solid fa-trash"></i>`;
+        newEle.innerHTML = `
+            <span class="task-text">${inputs.value}</span> 
+            <i class="fa-solid fa-trash" style="cursor: pointer; margin-left: 10px;"></i>
+        `;
         text.appendChild(newEle);
-        inputs.value="";
-        newEle.querySelector("i").addEventListener("click" , remove);
-        function remove(){
-            newEle.remove()
-        }
+        let taskText = newEle.querySelector(".task-text");
+        taskText.addEventListener('click', function () {
+            if (taskText.style.textDecoration === 'line-through') {
+                taskText.style.textDecoration = 'none';
+            } else {
+                taskText.style.textDecoration = 'line-through';
+            }
+        });
+        taskText.addEventListener('dblclick', function () {
+            let editInput = document.createElement("input");
+            editInput.type = "text";
+            editInput.value = taskText.textContent;
+            taskText.replaceWith(editInput);
+            editInput.addEventListener("blur", function () {
+                taskText.textContent = editInput.value;
+                editInput.replaceWith(taskText);
+            });
+        });
+        newEle.querySelector("i").addEventListener("click", function () {
+            newEle.remove();
+        });
+
+        inputs.value = "";
     }
 }
